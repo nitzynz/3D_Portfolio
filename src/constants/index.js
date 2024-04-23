72626069221cf117dfb5d8204d1bb2476abb9f91
nitzynz <nitzy206@gmail.com>
2024-04-23 15:35:10 -0400

    Updated Details

--- src/constants/index.js
+++ src/constants/index.js
@@ -175,12 +175,12 @@ export const socialLinks = [
   {
     name: "GitHub",
     iconUrl: github,
-    link: "https://github.com/YourGitHubUsername",
+    link: "https://github.com/nitzynz/",
   },
   {
     name: "LinkedIn",
     iconUrl: linkedin,
-    link: "https://www.linkedin.com/in/YourLinkedInUsername",
+    link: "https://www.linkedin.com/in/nithya-g/",
   },
 ];
 
